8b21873150680739cc2e11b1d3f4ef51dc5cc86a
Juliana Matos Felipe <julianamatosfelipe@gmail.com>
2021-10-05 20:40:07 -0300

    EXERCICIO 6 FINALIZADO

--- exercicio6.js
+++ exercicio6.js
@@ -1,39 +1,24 @@
-/*
-var nome = "Mariazinha"
-var nota1 = 6
-var nota2 = 7
-
-var resultado = (nota1 + nota2) / 2
-
-if (resultado >= 6) {
-    console.log(nome + " Foi aprovada")
-}
-else{
-    console.log(nome + " Foi reprovada")
-}
-*/
-
-var nota1 = 6
+var nota1 = 9
 var nota2 = 6
 
 var resultado = (nota1 + nota2) / 2
 
 if (resultado > 9.0){
-    console.log(nota1 + " " + nota2 + " " + resultado + " A" + " APROVADO")
+    console.log("N1:" + nota1 + " N2:" + nota2 + " Média:" + resultado + " Conseito:A" + " APROVADO")
 }
 
 else if (resultado > 7.5 && resultado <= 9.0){
-    console.log(nota1 + " " + nota2 + " " + resultado + " B" + " APROVADO")
+    console.log("N1:" + nota1 + " N2:" + nota2 + " Média:" + resultado + " Conseito:B" + " APROVADO")
 }
 
 else if (resultado > 6.0 && resultado <= 7.5){
-    console.log(nota1 + " " + nota2 + " " + resultado + " C" + " APROVADO")
+    console.log("N1:" + nota1 + " N2:" + nota2 + " Média:" + resultado + " Conceito:C" + " APROVADO")
 }
 
 else if (resultado >4.0 && resultado <= 6.0){
-    console.log(nota1 + " " + nota2 + " " + resultado + " D" + " REPROVADO")
+    console.log("N1:" + nota1 + " N2:" + nota2 + " Média:" + resultado + " Conseito:D" + " REPROVADO")
 }
 
 else if (resultado <= 4.0){
-    console.log(nota1 + " " + nota2 + " " + resultado + " E" + " REPROVADO")
+    console.log("N1:" + nota1 + " N2:" + nota2 + " Média:" + resultado + " Conseito:E" + " REPROVADO")
 }
