43d7c3b7e58e2797d1e6bf03b03ef7b252026253
Juliana Matos Felipe <julianamatosfelipe@gmail.com>
2021-10-05 21:26:14 -0300

    resolução

---
+++ exercicio5.js
@@ -0,0 +1,33 @@
+var dia = 5
+
+if (dia == 1){
+    console.log("Domingo")
+}
+
+else if (dia == 2){
+    console.log("Segunda")
+}
+
+else if (dia == 3){
+    console.log("Terça-feira")
+}
+
+else if (dia == 4){
+    console.log("Quarta-feira")
+}
+
+else if (dia == 5){
+    console.log("Quinta-feira")
+}
+
+else if (dia == 6){
+    console.log("Sexta-feira")
+}
+
+else if (dia == 7){
+    console.log("Sábado")
+}
+
+else{
+    console.log("Valor inválido")
+}
